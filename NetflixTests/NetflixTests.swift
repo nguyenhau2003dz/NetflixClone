9a6c5c93d287be21cab082eff10401b5e29e69a0
nguyenhau2003dz <nguyenduchaudeptrai2003@gmail.com>
2025-03-13 19:11:49 +0700

    Initial Commit

---
+++ NetflixTests/NetflixTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixTests.swift
+//  NetflixTests
+//
+//  Created by Hậu Nguyễn on 13/3/25.
+//
+
+import Testing
+@testable import Netflix
+
+struct NetflixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
